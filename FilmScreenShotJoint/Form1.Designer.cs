bf440af049b20bb085d21122299bc9da2da4717e
rongzhisheng <315989375@qq.com>
2018-12-06 13:00:54 +0800

    first commit

---
+++ FilmScreenShotJoint/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace FilmScreenShotJoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBoxOriginalImage = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonPreview = new System.Windows.Forms.Button();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.groupBoxPreview = new System.Windows.Forms.GroupBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.panelPreview = new System.Windows.Forms.Panel();
+            this.radioButtonZoom = new System.Windows.Forms.RadioButton();
+            this.radioButtonAutoSize = new System.Windows.Forms.RadioButton();
+            this.groupBoxOriginalImage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.groupBoxPreview.SuspendLayout();
+            this.panelPreview.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 17);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(544, 593);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // groupBoxOriginalImage
+            // 
+            this.groupBoxOriginalImage.Controls.Add(this.flowLayoutPanel1);
+            this.groupBoxOriginalImage.Location = new System.Drawing.Point(12, 37);
+            this.groupBoxOriginalImage.Name = "groupBoxOriginalImage";
+            this.groupBoxOriginalImage.Size = new System.Drawing.Size(550, 613);
+            this.groupBoxOriginalImage.TabIndex = 2;
+            this.groupBoxOriginalImage.TabStop = false;
+            this.groupBoxOriginalImage.Text = "原图列表";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG文件(*.jpg)|*.jpg|*.jpeg|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "请选择截图";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "JPG文件(*.jpg)|*.jpg|*.jpeg|*.*";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonOpen.Location = new System.Drawing.Point(12, 8);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(92, 23);
+            this.buttonOpen.TabIndex = 3;
+            this.buttonOpen.Text = "选择文件...";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonPreview
+            // 
+            this.buttonPreview.Location = new System.Drawing.Point(701, 621);
+            this.buttonPreview.Name = "buttonPreview";
+            this.buttonPreview.Size = new System.Drawing.Size(84, 26);
+            this.buttonPreview.TabIndex = 4;
+            this.buttonPreview.Text = "预览";
+            this.buttonPreview.UseVisualStyleBackColor = true;
+            this.buttonPreview.Click += new System.EventHandler(this.buttonPreview_Click);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(580, 552);
+            this.pictureBoxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPreview.TabIndex = 5;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // groupBoxPreview
+            // 
+            this.groupBoxPreview.Controls.Add(this.panelPreview);
+            this.groupBoxPreview.Location = new System.Drawing.Point(580, 37);
+            this.groupBoxPreview.Name = "groupBoxPreview";
+            this.groupBoxPreview.Size = new System.Drawing.Size(592, 578);
+            this.groupBoxPreview.TabIndex = 6;
+            this.groupBoxPreview.TabStop = false;
+            this.groupBoxPreview.Text = "结果预览";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(871, 621);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(84, 26);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // panelPreview
+            // 
+            this.panelPreview.AutoScroll = true;
+            this.panelPreview.Controls.Add(this.pictureBoxPreview);
+            this.panelPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPreview.Location = new System.Drawing.Point(3, 17);
+            this.panelPreview.Name = "panelPreview";
+            this.panelPreview.Size = new System.Drawing.Size(586, 558);
+            this.panelPreview.TabIndex = 6;
+            // 
+            // radioButtonZoom
+            // 
+            this.radioButtonZoom.AutoSize = true;
+            this.radioButtonZoom.Checked = true;
+            this.radioButtonZoom.Location = new System.Drawing.Point(580, 15);
+            this.radioButtonZoom.Name = "radioButtonZoom";
+            this.radioButtonZoom.Size = new System.Drawing.Size(59, 16);
+            this.radioButtonZoom.TabIndex = 8;
+            this.radioButtonZoom.TabStop = true;
+            this.radioButtonZoom.Text = "缩略图";
+            this.radioButtonZoom.UseVisualStyleBackColor = true;
+            this.radioButtonZoom.CheckedChanged += new System.EventHandler(this.radioButtonZoom_CheckedChanged);
+            // 
+            // radioButtonAutoSize
+            // 
+            this.radioButtonAutoSize.AutoSize = true;
+            this.radioButtonAutoSize.Location = new System.Drawing.Point(681, 15);
+            this.radioButtonAutoSize.Name = "radioButtonAutoSize";
+            this.radioButtonAutoSize.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonAutoSize.TabIndex = 9;
+            this.radioButtonAutoSize.TabStop = true;
+            this.radioButtonAutoSize.Text = "原图";
+            this.radioButtonAutoSize.UseVisualStyleBackColor = true;
+            this.radioButtonAutoSize.CheckedChanged += new System.EventHandler(this.radioButtonAutoSize_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 662);
+            this.Controls.Add(this.radioButtonAutoSize);
+            this.Controls.Add(this.radioButtonZoom);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.groupBoxPreview);
+            this.Controls.Add(this.buttonPreview);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.groupBoxOriginalImage);
+            this.Name = "Form1";
+            this.Text = "截图拼接小工具";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxOriginalImage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.groupBoxPreview.ResumeLayout(false);
+            this.panelPreview.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBoxOriginalImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonPreview;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.GroupBox groupBoxPreview;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Panel panelPreview;
+        private System.Windows.Forms.RadioButton radioButtonZoom;
+        private System.Windows.Forms.RadioButton radioButtonAutoSize;
+    }
+}
+
---
+++ FilmScreenShotJoint/Form1.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilmScreenShotJoint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            showError += new ShowError(ShowErrorByConsole);
+            showError += new ShowError(ShowErrorByMessageBox);
+            pictureBoxPreviewInitSize = pictureBoxPreview.Size;
+        }
+
+        private Size pictureBoxPreviewInitSize;
+        const int panelWidth = 526;
+        const int panelHeight = 284;
+        const int numericUpDownWidth = 50;
+        const int numericUpDownHeight = 21;
+        const int pictureBoxWidth = 467;
+        /// <summary>
+        /// 数字选择器的量程
+        /// 
+        /// </summary>
+        private int limitLength;
+        /// <summary>
+        /// 默认截图区域顶部位置
+        /// </summary>
+        private int defaultLimitTop;
+        /// <summary>
+        /// 默认截图高度
+        /// </summary>
+        private int minSubtitleHeight;
+        private int initExtraSubtitleHeight = 40;
+
+        private const int topFlag = 1, bottomFlag = 2;
+
+        private List<Image> imageList = new List<Image>();
+        /// <summary>
+        /// 存储用户选择的图片高度区间的列表<top, bottom>
+        /// </summary>
+        private List<LowerUpper> imageHeightSelectionList = new List<LowerUpper>();
+        // 图片实际宽度和高度（多张图片宽高要一致）
+        private int ImageRealWidth, imageRealHeight;
+
+        private delegate void ShowError(string msg);
+        private ShowError showError;
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            foreach (Image img in imageList)
+            {
+                img.Dispose();
+            }
+            imageList.Clear();
+            imageHeightSelectionList.Clear();
+            ClearPreview();
+        }
+
+        private void ClearPreview()
+        {
+            if (pictureBoxPreview.Image != null)
+            {
+                pictureBoxPreview.Image.Dispose();
+                pictureBoxPreview.Image = null;
+            }
+        }
+
+        private void EnablePreview(bool enable)
+        {
+            buttonPreview.Enabled = enable;
+            if (!enable)
+            {
+                EnablePreviewModeAndSave(enable);
+            }
+        }
+
+        private void EnablePreviewModeAndSave(bool enable)
+        {
+            buttonSave.Enabled = 
+            radioButtonAutoSize.Enabled = 
+            radioButtonZoom.Enabled = enable;
+        }
+
+        private void SetLimit(int limitLen)
+        {
+            limitLength = limitLen;
+            defaultLimitTop = (int)(limitLength * 0.78);
+            minSubtitleHeight = Math.Min((int)(limitLength * 0.1), 40);
+        }
+
+        /// <summary>
+        /// 点击选择文件按钮后的事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            Clear();
+
+            flowLayoutPanel1.Focus();
+
+            ImageRealWidth = -1;
+            imageRealHeight = -1;
+            string[] selectedImageFiles = openFileDialog1.FileNames;
+            foreach(string fileName in selectedImageFiles)
+            {
+                Console.WriteLine(fileName);
+                ////通过输入文件目录，文件模式，访问模式等参数，通过流打开文件
+                //FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                ////通过调用系统的画笔工具，画出一个Image类型的数据，传给pictureBox。
+                //Image image = Bitmap.FromStream(fs);
+                Image image = Image.FromFile(fileName);
+                if (ImageRealWidth < 0)
+                {
+                    ImageRealWidth = image.Width;
+                    imageRealHeight = image.Height;
+                    SetLimit(imageRealHeight);
+                    imageList.Add(image);
+                }
+                else if (ImageRealWidth != image.Width || imageRealHeight != image.Height)
+                {
+                    showError("图片尺寸不一致！");
+                    return;
+                }
+                else
+                {
+                    imageList.Add(image);
+                }
+                    
+            }
+            showImageList();
+            EnablePreview(true);
+            EnablePreviewModeAndSave(false);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            if (pictureBoxPreview.Image != null)
+            {
+                pictureBoxPreview.Image.Save(saveFileDialog1.FileName);
+            }    
+        }
+
+        /// <summary>
+        /// 将选中的图片展示出来
+        /// </summary>
+        private void showImageList()
+        {
+            int i = 0;
+            foreach(Image img in imageList)
+            {
+                showAImage(img, i++);
+            }
+        }
+
+        private void showAImage(Image img, int index)
+        {
+            #region pictureBox
+            PictureBox pictureBox = new PictureBox
+            {
+                BackColor = SystemColors.Window,
+                Dock = DockStyle.Left,
+                Location = new Point(0, 0),
+                Size = new Size(pictureBoxWidth, panelHeight),
+                Name = "pictureBox" + index,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Image = img
+            };
+            pictureBox.Click += new EventHandler(panel_Click);
+            #endregion
+
+            #region numericUpDown
+            NumericUpDown top = new NumericUpDown
+            {
+                Location = new Point(panelWidth - numericUpDownWidth, 0),
+                Size = new Size(numericUpDownWidth, numericUpDownHeight),
+                TabIndex = topFlag,
+                Minimum = 1,
+                Maximum = defaultLimitTop,
+                Value = Math.Max(defaultLimitTop - initExtraSubtitleHeight / 2, 1)
+            };
+            top.ValueChanged += new EventHandler(numericUpDownValueChanged);
+
+            NumericUpDown bottom = new NumericUpDown
+            {
+                Location = new Point(panelWidth - numericUpDownWidth, panelHeight - numericUpDownHeight),
+                Size = new Size(numericUpDownWidth, numericUpDownHeight),
+                TabIndex = bottomFlag,
+                Minimum = defaultLimitTop + minSubtitleHeight,
+                Maximum = limitLength,
+                Value = Math.Min(defaultLimitTop + minSubtitleHeight + initExtraSubtitleHeight / 2, limitLength)
+            };
+            bottom.ValueChanged += new EventHandler(numericUpDownValueChanged);
+            #endregion
+
+            #region panel      
+            Color coverPanelColor = Color.LightBlue;
+            Panel panelTop = new Panel
+            {
+                BackColor = coverPanelColor,
+                Location = new Point(0, 0),
+                Name = "panelTop" + index,
+                Size = new Size(pictureBoxWidth, panelHeight * (int)top.Value / limitLength),
+                TabIndex = topFlag
+            };
+
+            Panel panelBottom = new Panel
+            {
+                BackColor = coverPanelColor,
+                Location = new Point(0, panelHeight * (int)bottom.Value / limitLength),
+                Name = "panelBottom" + index,
+                Size = new Size(pictureBoxWidth, panelHeight * (limitLength - (int)top.Value) / limitLength),
+                TabIndex = bottomFlag
+            };
+
+            Panel panel = new Panel
+            {
+                BackColor = SystemColors.ActiveBorder,
+                Location = new Point(8, 8),
+                Name = "panel" + index,
+                Size = new Size(panelWidth, panelHeight),
+                TabIndex = 0
+            };
+            panel.Controls.Add(top);
+            panel.Controls.Add(bottom);
+            panel.Controls.Add(panelTop);// 先加的显示在上层
+            panel.Controls.Add(panelBottom);// 先加的显示在上层
+            panel.Controls.Add(pictureBox);
+            panel.Click += new EventHandler(panel_Click);
+            
+            
+
+            //using (Graphics g = panel.CreateGraphics())
+            //{
+            //    Brush brush = Brushes.Red; ;
+            //    g.FillRectangles(brush, new Rectangle[]
+            //    {
+            //        new Rectangle(0, 0, panelWidth, panelHeight * defaultMax / (int)top.Value),
+            //        new Rectangle(0, panelHeight * defaultMax / (int)bottom.Value, panelWidth, panelHeight * defaultMax/(defaultMax-(int)bottom.Value))
+            //    });
+            //}
+
+            
+
+            flowLayoutPanel1.Controls.Add(panel);
+            #endregion
+        }
+
+
+        private void numericUpDownValueChanged(object sender, EventArgs e)
+        {
+            if (!(sender is NumericUpDown))
+            {
+                return;
+            }
+            NumericUpDown src = (NumericUpDown)sender, dest;
+            Panel panel;
+            // 上面
+            if (src.TabIndex == topFlag)
+            {
+                dest = GetInParent<NumericUpDown>(src.Parent, bottomFlag);
+                if (dest == null)
+                {
+                    showError("dest is null");
+                    return;
+                }
+                src.Maximum = Math.Max(src.Maximum, dest.Value);
+                dest.Minimum = src.Value + minSubtitleHeight;// todo 如果dest的值小于最小值，可能需要程序调整
+                panel = GetInParent<Panel>(src.Parent, topFlag);
+                if (panel == null)
+                {
+                    showError("panel is null");
+                    return;
+                }
+                panel.Size = new Size(pictureBoxWidth, panelHeight * (int)src.Value / limitLength);
+            }
+            else// 下面
+            {
+                dest = GetInParent<NumericUpDown>(src.Parent, topFlag);
+                if (dest == null)
+                {
+                    showError("dest is null");
+                    return;
+                }
+                src.Minimum = Math.Min(src.Minimum, dest.Value);
+                dest.Maximum = src.Value - minSubtitleHeight;// todo 如果dest的值小于最小值，可能需要程序调整
+                panel = GetInParent<Panel>(src.Parent, bottomFlag);
+                if (panel == null)
+                {
+                    showError("panel is null");
+                    return;
+                }
+                panel.Size = new Size(pictureBoxWidth, panelHeight * (limitLength - (int)src.Value) / limitLength);
+                panel.Location = new Point(0, panelHeight * (int)src.Value / limitLength);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnablePreview(false);
+        }
+
+        private void panel_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Focus();
+            Console.WriteLine("flowLayoutPanel1.Focus");
+        }
+
+        private void buttonPreview_Click(object sender, EventArgs e)
+        {
+            imageHeightSelectionList.Clear();
+            foreach (Control c in flowLayoutPanel1.Controls)
+            {
+                if (c is Panel)// 遍历每一个截图面板
+                {
+                    NumericUpDown top = GetInParent<NumericUpDown>(c, topFlag);
+                    NumericUpDown bottom = GetInParent<NumericUpDown>(c, bottomFlag);
+                    if (top == null || bottom == null)
+                    {
+                        showError("can not found limit");
+                        return;
+                    }
+                    int lower = imageRealHeight * (int)top.Value / limitLength;
+                    int upper = imageRealHeight * (int)bottom.Value / limitLength;
+                    Console.WriteLine($"lower:{lower}, upper:{upper}");
+                    imageHeightSelectionList.Add(new LowerUpper(lower, upper));
+                }
+            }
+            GenerateImageAndShow();
+            EnablePreviewModeAndSave(true);
+        }
+
+        private void GenerateImageAndShow()
+        {
+            int totalHeight = 0;
+            for (int i = 0; i < imageHeightSelectionList.Count; i++)
+            {
+                LowerUpper range = imageHeightSelectionList[i];
+                if (i == 0)
+                {
+                    totalHeight += range.upper;
+                }
+                else
+                {
+                    totalHeight += range.upper - range.lower + 1;
+                }
+            }
+            Bitmap bitmap = new Bitmap(ImageRealWidth, totalHeight);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.DrawImage(bitmap, bitmap.Width, bitmap.Height);
+            int currentHeight = 0;
+            for (int i=0;i<imageList.Count;i++)
+            {
+                LowerUpper range = imageHeightSelectionList[i];
+               
+                Image img = imageList[i];
+                int y, height;
+                if (i == 0)
+                {
+                    y = 0;
+                    height = range.upper;
+                }
+                else
+                {
+                    y = range.lower - 1;
+                    height = range.upper - range.lower + 1;
+                }
+                // 摘要:在指定的位置绘制图像的一部分。
+                // 参数:
+                //   image:要绘制的 System.Drawing.Image。
+                //   x:所绘制图像的左上角的 x 坐标。
+                //   y:所绘制图像的左上角的 y 坐标。
+                //   srcRect:System.Drawing.Rectangle 结构，它指定 image 对象中要绘制的部分。
+                //   srcUnit:System.Drawing.GraphicsUnit 枚举的成员，它指定 srcRect 参数所用的度量单位。
+                // public void DrawImage(Image image, int x, int y, Rectangle srcRect, GraphicsUnit srcUnit);
+                g.DrawImage(img, 0, currentHeight, new Rectangle(0, y, ImageRealWidth, height), GraphicsUnit.Pixel);
+                currentHeight += height;
+            }
+            g.Dispose();
+            ClearPreview();
+            Console.WriteLine($"bitmap.Width: {bitmap.Width}, bitmap.Height:{bitmap.Height}, panelPreview.Size:{panelPreview.Size}");
+            pictureBoxPreview.Image = bitmap;
+            panelPreview.Focus();
+        }
+
+        private T GetInParent<T>(Control parent, int flag) where T : Control
+        {
+            foreach (Control c in parent.Controls)
+            {
+                if (c is T && c.TabIndex == flag)
+                {
+                    return (T)c;
+                }
+            }
+            return null;
+        }
+        
+        private void ShowErrorByConsole(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private void radioButtonZoom_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPreview.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxPreview.Location = new Point(0, 0);
+            pictureBoxPreview.Size = pictureBoxPreviewInitSize;
+            panelPreview.Focus();
+        }
+
+        private void radioButtonAutoSize_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxPreview.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBoxPreview.Location = new Point(0, 0);
+            panelPreview.Focus();
+        }
+
+        private void ShowErrorByMessageBox(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        struct LowerUpper
+        {
+            public int lower, upper;
+            public LowerUpper(int l, int u)
+            {
+                lower = l;
+                upper = u;
+            }
+        }
+    }
+}
